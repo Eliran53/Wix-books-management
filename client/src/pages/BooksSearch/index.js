47c0bc6f1a9cbfd208cd5109b4bee086e82540e8
eliran <elirantarka7@gmail.com>
2020-10-20 15:28:11 +0300

    add search component

--- client/src/pages/BooksSearch/index.js
+++ client/src/pages/BooksSearch/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Search from '../../components/Search/Search'
 
 export const BooksSearch = () => {
+
   return (
     <section>
       <h1>Search books</h1>
+      <Search/>
     </section>
+    
   );
-};
+  };
+
