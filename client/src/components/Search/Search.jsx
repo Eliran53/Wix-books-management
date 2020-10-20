12510988172a9df7f42adee288820d17369722f2
eliran <elirantarka7@gmail.com>
2020-10-20 15:27:52 +0300

    component for search

---
+++ client/src/components/Search/Search.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { TextField, Button } from "@material-ui/core";
+import { searchBooks, getBookCoverByOLID } from "../../BooksApi/index";
+import Card from "@material-ui/core/Card";
+import { makeStyles } from "@material-ui/core/styles";
+import { CardHeader, CardMedia, CardContent } from "@material-ui/core";
+
+function Search() {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      maxWidth: 345,
+      border:"solid"
+    },
+    media: {
+      height: 0,
+      paddingTop: "56.25%", // 16:9
+    },
+    expand: {
+      transform: "rotate(0deg)",
+      marginLeft: "auto",
+      transition: theme.transitions.create("transform", {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: "rotate(180deg)",
+    },
+  }));
+  const classes = useStyles();
+  const [books, setBooks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [pending, setPending] = useState("");
+
+  const onInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  async function onSubmit() {
+    setPending("searching...");
+    const result = await searchBooks(searchTerm);
+    setBooks(result.docs);
+    setPending("");
+  }
+  return (
+    <div>
+      <TextField
+        variant="outlined"
+        placeholder="search book by name"
+        size="small"
+        value={searchTerm}
+        onChange={onInputChange}
+      />{" "}
+      &nbsp;
+      <Button
+        variant="contained"
+        color="primary"
+        size="medium"
+        onClick={onSubmit}
+      >
+        search
+      </Button>
+      <div>{pending}</div>
+      {books.map((book) => (
+          <div><br/><br/>
+        <Card className={classes.root}>
+          <CardHeader title={book.title}></CardHeader>
+          <CardMedia className={classes.media}
+        
+        title={book.title}
+            image={getBookCoverByOLID(book.cover_edition_key)}
+          ></CardMedia>
+          <CardContent>{book.first_publish_year}<br/>{book.author_name}</CardContent>
+        </Card> 
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Search;
