2e13c44c823ca680de2bb331c688f0682b861ee5
eliran <elirantarka7@gmail.com>
2020-10-22 23:39:43 +0300

    added context

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import * as style from "./App.module.scss";
 import { Switch, Route } from "react-router-dom";
 import { Collections } from "./pages/Collections";
 import { BooksSearch } from "./pages/BooksSearch";
 import { Header } from "./components/Header";
+import { UserProvider } from "./Context";
 
 const App = () => {
+  //  const test = [{name:"test",bookname:"lord"},{name:"test2",bookname:"harry"}]
+  const [collectionsName, setCollectionsName] = useState([]);
+  const [collectionsBooks, setCollectionsBook] = useState([]);
+  const providerOptions = {
+    collectionsData: collectionsName,
+    booksdata: collectionsBooks,
+    changeCollectionData: (value) => {
+      const list = {name:value,books:[],images:[]};
+      collectionsName.push(list);
+      setCollectionsName(collectionsName);
+    },
+    changeBooksData: (value) => {
+      collectionsBooks.push(value);
+      setCollectionsBook(collectionsBooks);
+    },
+  };
+
   return (
     <div className={style.app}>
-      <Header />
-      <main>
-        <Switch>
-          <Route
-            exact
-            path="/search"
-            render={(props) => (
-              <BooksSearch {...props} />
-            )}
-          />
-          <Route
-            exact
-            path="/collections"
-            render={(props) => (<Collections {...props}/>)}
-          />
-          <Route
-            path="/"
-            render={(props) => (
-              <BooksSearch {...props}/>
-            )}
-          />
-        </Switch>
-      </main>
+      <UserProvider value={providerOptions}>
+        <Header />
+        <main>
+          <Switch>
+            <Route
+              exact
+              path="/search"
+              render={(props) => <BooksSearch {...props} />}
+            />
+            <Route
+              exact
+              path="/collections"
+              render={(props) => <Collections {...props} />}
+            />
+            <Route path="/" render={(props) => <BooksSearch {...props} />} />
+          </Switch>
+        </main>
+      </UserProvider>
     </div>
   );
-}
+};
 
 export default App;
