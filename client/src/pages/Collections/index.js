f09c2e5932fa0b7bb12ecc61c5e4670a8a7b88fb
eliran <elirantarka7@gmail.com>
2020-10-22 23:41:27 +0300

    component for collections

--- client/src/pages/Collections/index.js
+++ client/src/pages/Collections/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import * as style from "./Collections.module.scss";
+import AddCollection from '../../components/BooksCollections/addcollection'
 
-export const Collections = () => {
+export const Collections = (props) => {
   return (
     <section>
       <h1>Books collections</h1>
+      <AddCollection/>
     </section>
   );
 };
